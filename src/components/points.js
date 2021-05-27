f2722b2fb3316c7bba1391caaaffc3c968911e05
מלכה מאיר <323947184@mbydomain>
2021-05-27 13:33:13 +0300

    report

--- src/components/points.js
+++ src/components/points.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
 
-const Points = ({ picture, note, isPointKnown }) => {
+const Points = ({ picture,isPointKnown,note,name}) => {
    
     const [isknown, setIsknown] = useState(isPointKnown);
     const [isnote, setIsnote] = useState(note);
@@ -10,22 +10,23 @@ const Points = ({ picture, note, isPointKnown }) => {
     return (
     <div>
         <img src={""} />
+        <br/>
+        <br/>
         <Switch
             checked={isknown}
             onChange={(e)=>setIsknown(e.target.checked)}
             inputProps={{ 'aria-label': 'secondary checkbox' }}
         />
-  
+        <br/>
+        <br/>
         <TextField
           id="standard-multiline-static"
-          label="Multiline"
+          label={name}  
           multiline
           rows={4}
-          defaultValue="הערה כוללת"
+          defaultValue=""
           onChange={(e)=>setIsnote(e.target.value)}
         />
-        
-  
     </div>)
 }
 
--- src/components/report.js
+++ src/components/report.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import Points from './points'
 import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+
 
 const Report = () => {
 const pointsArr = [{ name: "קמץ", isKnown: false, picture: "images/....png" }, { name: "פתח", isKnown: true, picture: "images/....png" }];
     return (      
         <div>
-            {pointsArr.map((point) => <Points picture={""} isPointKnown={point.isKnown} note={point.isnote}  />)}
+            {pointsArr.map((point) => <Points picture={""} isPointKnown={point.isKnown} note={point.isnote} name={point.name}  />)}
             
-   
+            <TextField
+            id="standard-multiline-static"
+            label="הערה כללית"
+            multiline
+            rows={4}
+            defaultValue=""
+            onChange={(e)=>(e.target.value)}
+            />
+            <br/>
+            <br/>
+            <br/>
             <Button>שמירה</Button>
         </div>
     )

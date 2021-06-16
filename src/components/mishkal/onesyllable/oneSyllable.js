b0f86a578964be6d86f5a9163f0f6f8daa393892
מלכה מאיר <323947184@mbydomain>
2021-06-16 14:55:57 +0300

    fetch

--- src/components/mishkal/onesyllable/oneSyllable.js
+++ src/components/mishkal/onesyllable/oneSyllable.js
@@ -1,38 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 //import { useHistory } from "react-router-dom";
 import Word from './word'
+import ShoWord from '../shoWord'
 
 const OneSyllable = () => {
     //     let history=useHistory();
     //     const id="";
-
-    const kamatz = () => {
-        return (<Word id="kamatz" />)
-    }
-    const chirik = () => {
-        return (<Word id="chirik" />)
-
-    }
-    const cholam = () => {
-        return (<Word id="cholam" />)
-
-    }
-    const segol = () => {
-        return (<Word id="segol" />)
-
-    }
-    const shuruk = () => {
-        return (<Word id="shuruk" />)
-
-    }
+    const[nikud,setNikud]=useState('')
+    useEffect{[
+   
+        fetch('localhost8080/wordsController/' + nikud)
+        .then(response => response.json())
+        .then(data => <ShoWord data={data}/>)
+    ],nikud}
+    // const chirik = () => {
+    //     return (<Word id="chirik" />)
+    // }
+    // const cholam = () => {
+    //     return (<Word id="cholam" />)
+    // }
+    // const segol = () => {
+    //     return (<Word id="segol" />)
+    // }
+    // const shuruk = () => {
+    //     return (<Word id="shuruk" />)
+    // }
     // const sentences=()=>{
     //     history.push('/sentences')
-
     // }
     return (
-        <div>OneSyllable</div>
-
-
+        <div>
+        <button  onClick={()=>setNikud('kamatz')}>kamatz</button>
+        <button  onClick={()=>setNikud('chirik')}>kamatz</button>
+        <button  onClick={()=>setNikud('cholam')}>kamatz</button>
+        <button  onClick={()=>setNikud('segol')}>kamatz</button>
+        <button  onClick={()=>setNikud('shuruk')}>kamatz</button>
+        </div>
     )
 }
 export default OneSyllable;
--- src/components/mishkal/twoSyllable/differentVowel.js
+++ src/components/mishkal/twoSyllable/differentVowel.js
@@ -2,27 +2,41 @@ import React from 'react'
 import Word from '../onesyllable/word'
 
 const DifferentVowel = () => {
-    const gina = () => {
-        return (<Word id="gina" />)
-    }
-    const chani = () => {
-        return (<Word id="chani" />)
+    
+    // const gina = () => {
+    //     return (<Word id="gina" />)
+    // }
+    // const chani = () => {
+    //     return (<Word id="chani" />)
 
-    }
-    const ola = () => {
-        return (<Word id="ola" />)
+    // }
+    // const ola = () => {
+    //     return (<Word id="ola" />)
 
-    }
-    const tzura = () => {
-        return (<Word id="tzura" />)
+    // }
+    // const tzura = () => {
+    //     return (<Word id="tzura" />)
 
-    }
-    const mix = () => {
-        return (<Word id="mix" />)
+    // }
+    // const mix = () => {
+    //     return (<Word id="mix" />)
 
-    }
+    // }
+    const[nikud,setNikud]=useState('')
+    useEffect{[
+   
+        fetch('localhost8080/wordsController/' + nikud)
+        .then(response => response.json())
+        .then(data => <ShoWord data={data}/>)
+    ],nikud}
     return (
-        <div>differentVowel</div>
+        <div>
+        <button  onClick={()=>setNikud('gina')}>kamatz</button>
+        <button  onClick={()=>setNikud('chani')}>kamatz</button>
+        <button  onClick={()=>setNikud('ola')}>kamatz</button>
+        <button  onClick={()=>setNikud('tzura')}>kamatz</button>
+        <button  onClick={()=>setNikud('mix')}>kamatz</button>
+        </div>
     )
 }
 export default DifferentVowel;

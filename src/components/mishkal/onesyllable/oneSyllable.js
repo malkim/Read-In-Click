a317bc2f3f040ff4d94a6dd65d2a73267880bc97
מלכה מאיר <323947184@mbydomain>
2021-06-16 15:04:37 +0300

    again

--- src/components/mishkal/onesyllable/oneSyllable.js
+++ src/components/mishkal/onesyllable/oneSyllable.js
@@ -1,40 +1,24 @@
 import React, { useState, useEffect } from 'react'
-//import { useHistory } from "react-router-dom";
 import Word from './word'
 import ShoWord from '../shoWord'
 
 const OneSyllable = () => {
-    //     let history=useHistory();
-    //     const id="";
-    const[nikud,setNikud]=useState('')
-    useEffect{[
-   
-        fetch('localhost8080/wordsController/' + nikud)
-        .then(response => response.json())
-        .then(data => <ShoWord data={data}/>)
-    ],nikud}
-    // const chirik = () => {
-    //     return (<Word id="chirik" />)
-    // }
-    // const cholam = () => {
-    //     return (<Word id="cholam" />)
-    // }
-    // const segol = () => {
-    //     return (<Word id="segol" />)
-    // }
-    // const shuruk = () => {
-    //     return (<Word id="shuruk" />)
-    // }
-    // const sentences=()=>{
-    //     history.push('/sentences')
-    // }
+
+    const [nikud, setNikud] = useState('');
+    useEffect(() => {
+        fetch('localhost:3000/wordsController/' + nikud)
+            .then(response => response.json())
+            .then(data => <ShoWord data={data} />)
+    }, [nikud])
+
     return (
         <div>
-        <button  onClick={()=>setNikud('kamatz')}>kamatz</button>
-        <button  onClick={()=>setNikud('chirik')}>kamatz</button>
-        <button  onClick={()=>setNikud('cholam')}>kamatz</button>
-        <button  onClick={()=>setNikud('segol')}>kamatz</button>
-        <button  onClick={()=>setNikud('shuruk')}>kamatz</button>
+            <button onClick={() => setNikud('kamatz')}>kamatz</button>
+            <button onClick={() => setNikud('chirik')}>chirik</button>
+            <button onClick={() => setNikud('cholam')}>cholam</button>
+            <button onClick={() => setNikud('segol')}>segol</button>
+            <button onClick={() => setNikud('shuruk')}>shuruk</button>
+            hello
         </div>
     )
 }

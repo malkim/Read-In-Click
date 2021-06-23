c7f3ccc763079f2db254bfe4895325a99d09e9f0
מלכה מאיר <323947184@mbydomain>
2021-06-23 16:28:50 +0300

    ggg

--- src/components/mishkal/consonant/consonant.js
+++ src/components/mishkal/consonant/consonant.js
@@ -4,18 +4,21 @@ import ShoWord from '../shoWord'
 const OneSyllable = () => {
     const [nikud, setNikud] = useState('');
     useEffect(() => {
-        fetch('localhost:3000/wordsController/' + nikud)
+        fetch('localhost:6666/word/get' + nikud)
             .then(response => response.json())
             .then(data => <ShoWord data={data} />)
+            .catch(error=>{console.error(error)})
     }, [nikud])
 
     return (
         <div>
-            <button onClick={() => setNikud('kamatz')}>kamatz</button>
-            <button onClick={() => setNikud('chirik')}>chirik</button>
-            <button onClick={() => setNikud('cholam')}>cholam</button>
-            <button onClick={() => setNikud('segol')}>segol</button>
+            <button onClick={() => setNikud('migdal')}>migdal</button>
+             <button onClick={() => setNikud('targil')}>targil</button>
+            <button onClick={() => setNikud('machsev')}>machsev</button>
+            {/* <button onClick={() => setNikud('segol')}>segol</button>
             <button onClick={() => setNikud('shuruk')}>shuruk</button>
+            <button onClick={() => setNikud('shurukcholam')}>shuruk</button>
+            <button onClick={() => setNikud('chirikkamatz')}>chirik</button> */}
 
         </div>
     )
